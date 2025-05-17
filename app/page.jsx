afacba7d9546c736b9b1ba9f8e1073466dba3e64
bxbx1205 <srbhanarkar05@gmail.com>
2025-05-17 15:20:32 +0530

    modified:   app/page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,34 +3,30 @@ import Squares from '../components/Squares.jsx';
 import DiscordLanding from '../components/DiscordLanding.jsx';
 import InstagramLanding from '../components/InstagramLanding.jsx';
 
-
-
 export default function Home() {
   return (
-
     <>
-       <div className="app">
-      <DiscordLanding />
-      <InstagramLanding />
-    </div>
-    
+      <div style={{
+        position: 'fixed',
+        inset: 0,
+        zIndex: -1,
+        width: '100vw',
+        height: '100vh',
+        overflow: 'hidden'
+      }}>
+        <Squares 
+          speed={0.5} 
+          squareSize={40}
+          direction='down'
+          borderColor='#fff'
+          hoverFillColor='#ffa500'
+        />
+      </div>
 
-    <div style={{
-      position: 'fixed',
-      inset: 0,
-      zIndex: -1, // behind other content
-      width: '100vw',
-      height: '100vh',
-      overflow: 'hidden'
-    }}>
-      <Squares 
-        speed={0.5} 
-        squareSize={40}
-        direction='down'
-        borderColor='#fff'
-        hoverFillColor='#ffa500'
-      />
-    </div>
+      <div className="app">
+        <DiscordLanding />
+        <InstagramLanding />
+      </div>
     </>
   );
 }
